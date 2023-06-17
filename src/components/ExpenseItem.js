2f82d85bb0bd8d42064ed5274332dacbde720794
Ophir Bentov <wannabegurux@gmail.com>
2023-06-17 13:42:20 -0700

    added expense component, optimized app.js, added styling to component.

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,10 +5,10 @@ const ExpenseItem = (props) => {
  
   return (
     <div className='expense-item'>
-        <ExpenseDate date={props.expense.date}/>
+        <ExpenseDate date={props.date}/>
       <div className='expense-item__description'>
-        <h2>{props.expense.title}</h2>
-        <div className='expense-item__price'>${props.expense.amount}</div>
+        <h2>{props.title}</h2>
+        <div className='expense-item__price'>${props.amount}</div>
       </div>
     </div>
   );
