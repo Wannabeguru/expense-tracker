7d403d0660556fafc5a5b97f105875b74e60b5b4
Ophir Bentov <wannabegurux@gmail.com>
2023-06-26 15:19:39 -0700

    added functionality to filter button

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -1,25 +1,35 @@
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css'
+import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 const Expense = (props) => {
-  const [filteredYear, setFilteredYear] = useState('2020')
-  const filterChangeHandler = selectedYear => {
+  const [filteredYear, setFilteredYear] = useState("2020");
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
+
+  const filteredExpenses = props.items.filter(expense => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  })
   return (
-
     <div>
-     
-   
-    <Card className="expenses">
-      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
-      {props.items.map(expense => <ExpenseItem title={expense.title} amount={expense.amount} date={expense.date}/>)}
-    </Card>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {filteredExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+      </Card>
     </div>
-
   );
 };
 
